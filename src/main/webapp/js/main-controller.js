718ba43c987d31ad67d21780f4607eae5585bb0e
Mohit Suman <msuman@redhat.com>
2016-07-13 02:46:57 +0530

    angular controller for api call

---
+++ src/main/webapp/js/main-controller.js
@@ -0,0 +1,20 @@
+'use strict';
+
+appControllers.controller('MainController', ['$rootScope', '$scope', '$http', 'authorization',
+    function($rootScope, $scope, $http, authorization) {
+        $scope.status = 'running...';
+        $scope.profile = authorization.profile;
+        $scope.isAdmin = authorization.hasRealmRole('admin')
+        $scope.isManager = authorization.hasRealmRole('manager')
+
+        $scope.getContracts = function() {
+        	$http.get("http://localhost:8000/api/contracts").success(function(data) {
+            	$scope.contracts = data;
+            });
+        }
+
+        $scope.logout = function() {
+        	authorization.logout();
+        }
+    }
+]);
